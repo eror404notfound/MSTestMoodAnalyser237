64a37313751a46d47abeea96666b81a137217055
Ankit <ankitcool4370@gmail.com>
2023-01-04 21:47:25 +0530

    Performing UC 2

--- MSTestMoodAnalyser/UnitTest1.cs
+++ MSTestMoodAnalyser/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace MSTestMoodAnalyser
     {
         [TestMethod]
         [DataRow("I am in Sad Mood", "SAD")]     //TC 1.1    Given "I am in Sad Mood" should return "SAD" .
-        public void Return_Sad_Mood_ByMethod(string message, string expected)
+        public void GivenSad_ViaMethod_ReturnSad(string message, string expected)
         {
             //Arrange
             MoodAnalyser moodAnalyser = new MoodAnalyser();
@@ -21,7 +21,7 @@ namespace MSTestMoodAnalyser
         }
         [TestMethod]
         [DataRow("I am in Any Mood", "HAPPY")]     //TC 1.2    Given "I am in Any Mood" should return "HAPPY" .
-        public void Return_Happy_Mood_ByMethod(string message, string expected)
+        public void GivenAnyMood_ViaMethod_ReturnHappy(string message, string expected)
         {
             //Arrange
             MoodAnalyser moodAnalyser = new MoodAnalyser();
@@ -34,7 +34,7 @@ namespace MSTestMoodAnalyser
         }
         [TestMethod]
         [DataRow("I am in Sad Mood", "SAD")] //After Refactor Repeating TC 1.1, Given "I am in Sad Mood" should return "SAD" .
-        public void ViaConstructor_ReturnSad(string message, string expected)
+        public void GivenSad_ViaConstructor_ReturnSad(string message, string expected)
         {
             //Arrange
             MoodAnalyser moodAnalyser = new MoodAnalyser(message);//message passed via constructor
@@ -47,13 +47,26 @@ namespace MSTestMoodAnalyser
         }
         [TestMethod]
         [DataRow("I am in Any Mood", "HAPPY")] //After Refactor Repeating TC 1.2, Given "I am in Any Mood" should return "HAPPY" .
-        public void ViaConstructor_ReturnHappy(string message, string expected)
+        public void GivenAnyMood_ViaConstructor_ReturnHappy(string message, string expected)
         {
             //Arrange
             MoodAnalyser moodAnalyser = new MoodAnalyser(message);//message passed via constructor
 
             //Act
             string actualOutput = moodAnalyser.AnalyseMood();//message not passed via parameterised method.
+
+            //Assert
+            Assert.AreEqual(actualOutput, expected);
+        }
+        [TestMethod]
+        [DataRow(null, "Happy")] //TC 2.1, Given null should return "HAPPY" .
+        public void GivenNullMood_ViaConstructor_ReturnHappy(string message, string expected)
+        {
+            //Arrange
+            MoodAnalyser moodAnalyser = new MoodAnalyser(message);//message passed via constructor
+
+            //Act
+            string actualOutput = moodAnalyser.AnalyseMood();
 
             //Assert
             Assert.AreEqual(actualOutput, expected);
